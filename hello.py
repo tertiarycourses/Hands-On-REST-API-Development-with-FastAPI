c2bf208533136dc75fec84463f3bc80462a5d93c
macus123 <122253505+macus123@users.noreply.github.com>
2024-11-30 07:34:57 +0000

    amended to fit github tunnelling

--- hello.py
+++ hello.py
@@ -3,13 +3,20 @@
 
 from fastapi import FastAPI
 import uvicorn
+import requests
+import httpx
+import json
 
 app = FastAPI() # top-level FastAPI object that represents the whole webapp
 
 @app.get("/hi") # path decorator, indicates that a request for the URL "/hi" on this server should be directed to the following function
 def greet(): # path function AKA main point of contact with HTTP requests and responses
-    return "Hellow? World?"
+    return {"message": "Hello? World?"} # return message in a JSON format
 
 # run the webapp in a web server with Uvicorn
 if __name__ == "__main__":
     uvicorn.run("hello:app", reload=True) # reload=True will restart the web server if hello.py changes
+
+# for easy testing, just add /hi to the end of the codespace link
+# like this
+# https://solid-acorn-9vwvj9q9gqwfgwq-8000.app.github.dev/hi
---
+++ hello_test.py
@@ -0,0 +1,37 @@
+import requests
+import httpx
+"""
+For security, as Github Codespaces exposes the URL through a public link, for requests made to our server (which is hosted by GitHub by virtue of the machine that is currently running this Codespace)
+we will have to include our Session Token in here.
+You can find your session token using this command in the terminal:
+
+```
+echo $GITHUB_TOKEN
+```
+The tolen always starts with ghu_
+We can then use curl to access it with the token:
+```
+curl ADDRESS -H "X-Github-Token: TOKEN"
+```
+"""
+
+if __name__ == "__main__":
+    token = "ghu_zYd02NM6xRrHf1MSUWr6hwOUrPULqq0sa3Pq"
+    headers = {"X-GitHub-Token": token}
+    # -----TEST USING REQUESTS-----
+    r = requests.get(
+        "https://solid-acorn-9vwvj9q9gqwfgwq-8000.app.github.dev/hi",
+        headers=headers,
+    )
+    print("OUTPUT USING REQUESTS TEST")
+    print(r.status_code)  # Should print 200
+    print(r.json())       # Should print {"message": "Hello? World?"}
+
+    # -----TEST USING HTTPX-----
+    r = httpx.get(
+        "https://solid-acorn-9vwvj9q9gqwfgwq-8000.app.github.dev/hi",
+        headers=headers,
+    )
+    print("OUTPUT USING HTTPX TEST")
+    print(r.status_code)  # Should print 200
+    print(r.json())       # Should print {"message": "Hello? World?"}
